df522921d53e113c5d827f8725f13bd0447a82fd
Jon Taylor <jonnytest1101@icloud.com>
2022-04-12 23:06:24 -0500

    Init setup

---
+++ public/assets/javascript/main.js
@@ -0,0 +1,3 @@
+function openModal() {
+  document.querySelector('#modalId').classList.remove('hidden')
+}
---
+++ server.js
@@ -0,0 +1,26 @@
+global.__rootdir = __dirname
+const PORT = process.env.PORT || 3000
+
+import { join } from 'path'
+import express from 'express'
+import { sequelize, sessionCreated, hbs } from "./lib/config";
+
+import controllers from './controllers'
+
+const app = express()
+
+const setStaticPath = (path) => express.static(join(__rootdir, 'public', 'assets', path))
+
+app.use(sessionCreated)
+  .engine('hbs', hbs.engine)
+  .set('view engine', 'hbs')
+  .set('views', './views/pages')
+  .use(express.json())
+  .use(express.urlencoded({ extended: false }))
+  .use('/images', setStaticPath('images'))
+  .use('/js', setStaticPath('javascript'))
+  .use('/css', setStaticPath('stylesheets'))
+  .use('/', controllers)
+
+sequelize.sync({ force: false, logging: false })
+  .then(() => app.listen(PORT, () => console.log(`http://localhost:${PORT}`)))
